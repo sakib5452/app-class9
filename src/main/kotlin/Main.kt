a5010dac40d2c446a75426da681e94a4d794defa
najmul sakib <najmus191@gmail.com>
2023-11-28 12:44:54 +0600

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+fun main(args: Array<String>) {
+  sum()
+  sup()
+  multi()
+  div()
+  mul()
+}
+
+
+fun sum(){
+    var a = 20
+    var b = 8
+    var sum = a+b
+    println("Total Sup: ${sum}")
+}
+fun sup(){
+    var a = 20
+    var b = 8
+    var sup = a-b
+    println("Total Sup: ${sup}")
+}
+fun multi(){
+    var a = 20
+    var b = 8
+    var multi = a*b
+    println("Total Sup: ${multi}")
+}
+fun div(){
+    var a = 20
+    var b = 5
+    var div = a/b
+    println("Total Sup: ${div}")
+}fun mul(){
+    var a = 20
+    var b = 6
+    var mul = a%b
+    println("Total Sup: ${mul}")
+}

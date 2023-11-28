7acccd593df7c3e2c0d17829780a805d3ed41931
najmul sakib <najmus191@gmail.com>
2023-11-28 13:00:07 +0600

    edit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,7 +11,7 @@ fun sum(){
     var a = 20
     var b = 8
     var sum = a+b
-    println("Total Sup: ${sum}")
+    println("Total Sum: ${sum}")
 }
 fun sup(){
     var a = 20
@@ -23,16 +23,16 @@ fun multi(){
     var a = 20
     var b = 8
     var multi = a*b
-    println("Total Sup: ${multi}")
+    println("Total multi: ${multi}")
 }
 fun div(){
     var a = 20
     var b = 5
     var div = a/b
-    println("Total Sup: ${div}")
+    println("Total div: ${div}")
 }fun mul(){
     var a = 20
     var b = 6
     var mul = a%b
-    println("Total Sup: ${mul}")
+    println("Total mul: ${mul}")
 }
